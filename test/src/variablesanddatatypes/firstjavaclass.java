c7776021dabaf12c27cd5a42e01246fe70a577db
aishwarya <aishwaryatambe94@gmail.com>
2019-09-24 21:51:23 +0530

    variables and data types

---
+++ test/src/variablesanddatatypes/firstjavaclass.java
@@ -0,0 +1,27 @@
+package variablesanddatatypes;
+
+public class firstjavaclass {
+//widely used data types
+	public static void main(String[] args) {
+		
+		int a=10;//for integer values
+		char b='A';//for single characters
+		boolean c=true;//for boolean values
+		double d=6.89;//for decimal values
+		String name="Aishwarya";// for strings values ** string is not data types
+				
+				//other data types:
+				//for numbers
+				byte x=10;
+		short y=45;
+		long z=67562;
+		
+		//for decimal values
+		float f=67.8f;
+		
+		System.out.println(a);
+		
+				
+	}
+
+}
